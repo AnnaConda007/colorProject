b7f50367bd625d188c0ede1638a1d76fe0935b79
Anna Hrulckova <annahrulkova@yandex.ru>
2023-01-29 22:49:25 +0400

    все формы открываются

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
 // вызов функций
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
-  modal(".triger", ".form-substrate", ".closeBtn");
+  modals(".triger-blue", ".form-substrate-blue", ".closeBtn");
+  modals(".triger-pink", ".form-substrate-pink", ".closeBtn");
+  modals(".triger-red", ".form-substrate-red", ".closeBtn");
 });
 
 // modals
-const modal = (trigerSelecrtor, modalSelector, closeSelector) => {
-  const openModalBtn = document.querySelectorAll(trigerSelecrtor);
-  const modal = document.querySelectorAll(form - substrate);
-  const closeModal = document.querySelectorAll(closeBtn);
+const modals = (trigerSelecrtor, modalSelector, closeSelector) => {
+  const openTrigers = document.querySelectorAll(trigerSelecrtor);
+  const modal = document.querySelector(modalSelector);
+  const closeModals = document.querySelectorAll(closeSelector);
 
-  // не забудь, что используется массив querySelectorAll
+  openTrigers.forEach((triger) => {
+    triger.addEventListener("click", () => {
+      modal.style.display = "block";
+    });
+  });
+
+  closeModals.forEach((close) => {
+    close.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+  });
 };
+
+/*
+
+
+
+*/
