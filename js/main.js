e211516f8b61b995fbef67fc2a7a74da4ebcbf44
Anna Hrulckova <annahrulkova@yandex.ru>
2023-01-29 20:49:56 +0400

    работа с модальными окнами

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 // вызов функций
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
+  modal(".triger", ".form-substrate", ".closeBtn");
 });
 
 // modals
-const modal = () => {};
+const modal = (trigerSelecrtor, modalSelector, closeSelector) => {
+  const openModalBtn = document.querySelectorAll(trigerSelecrtor);
+  const modal = document.querySelectorAll(form - substrate);
+  const closeModal = document.querySelectorAll(closeBtn);
+
+  // не забудь, что используется массив querySelectorAll
+};
