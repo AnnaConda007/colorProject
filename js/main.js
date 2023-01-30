d88cc3e12e9dbfaac1c0bb666c1eaa26370bf23d
Anna Hrulckova <annahrulkova@yandex.ru>
2023-01-30 22:58:03 +0400

    фильтрация цветов

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
 // вызов функций
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
+  filterBlock();
 });
 
 // modals
 const modal = () => {};
+
+// фильтр элементов
+
+const filterBlock = () => {
+  const allBtn = document.querySelector(".all-collors");
+
+  const showColor = (btn, showSelector) => {
+    document.querySelectorAll(".color-wrap div").forEach((div) => {
+      div.style.opacity = 1;
+    });
+
+    document.querySelector(btn).addEventListener("click", () => {
+      document
+        .querySelectorAll(`.color-wrap div :not(${showSelector})`)
+        .forEach((div) => {
+          div.style.opacity = 0.1;
+        });
+
+      document.querySelectorAll(".pink-color").forEach((div) => {
+        div.style.width = "50px";
+      });
+    });
+  };
+
+  showColor(".pink", ".pink-color");
+  showColor(".blue", ".blue-color");
+  showColor(".yellow", ".yellow-color");
+  showColor(".green", ".pink-color");
+};
