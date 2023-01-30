9639e8297da1afe88520a739b6cd11a0cbc22ec7
Anna Hrulckova <annahrulkova@yandex.ru>
2023-01-30 20:43:20 +0400

    функции  toCloseBy toOpenBy адаптированы под открытие бургер меню

--- js/main.js
+++ js/main.js
@@ -4,26 +4,37 @@ window.addEventListener("DOMContentLoaded", () => {
   modalBlock(".triger-pink", ".form-substrate-pink", ".closeBtn");
 });
 
-// modalBlock
+// modalBlock и бургер-меню
 const modalBlock = (openSelector, modalSelector, closeSelector) => {
-  const open = document.querySelector(openSelector);
+  const openModal = document.querySelector(openSelector);
   const modal = document.querySelector(modalSelector);
   const close = document.querySelector(closeSelector);
 
-  const toCloseBy = (eventType, elem) => {
-    elem.addEventListener(eventType, (e) => {
-      if (e.target === modal || e.target === close || e.code == "Escape") {
-        modal.style.display = "none";
+  const toCloseBy = (eventType, closeBtn, closeElem) => {
+    closeElem =
+      closeElem[0] === "." ? document.querySelector(closeElem) : closeElem;
+    closeBtn =
+      closeBtn[0] === "." ? document.querySelector(closeBtn) : closeBtn;
+    closeBtn.addEventListener(eventType, (e) => {
+      if (e.target === modal || e.code == "Escape" || e.target === closeBtn) {
+        closeElem.style.display = "none";
       }
     });
   };
 
-  open.addEventListener("click", (e) => {
-    e.preventDefault();
-    modal.style.display = "block";
-  });
+  const toOpenBy = (triger, visible) => {
+    triger = triger[0] === "." ? document.querySelector(triger) : triger;
+    visible = visible[0] === "." ? document.querySelector(visible) : visible;
+    triger.addEventListener("click", () => {
+      visible.style.display = "block";
+    });
+  };
 
-  toCloseBy("click", close);
-  toCloseBy("click", window);
-  toCloseBy("keydown", window);
+  toOpenBy(openModal, modal);
+  toOpenBy(".menu-btn", ".menu");
+
+  toCloseBy("click", close, modal);
+  toCloseBy("click", window, modal);
+  toCloseBy("keydown", window, modal);
+  toCloseBy("click", ".closeMenu", ".menu");
 };
