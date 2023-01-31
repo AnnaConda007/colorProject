8d14905d0e9614d5a17eb97443b6d43085895018
Anna Hrulckova <annahrulkova@yandex.ru>
2023-01-31 13:32:17 +0400

    добавлена проверка на колличество элементов в переданном селекторе

--- js/main.js
+++ js/main.js
@@ -10,28 +10,25 @@ const modal = () => {};
 // фильтр элементов
 
 const filterBlock = () => {
-  const allBtn = document.querySelector(".all-collors");
-
   const showColor = (btn, showSelector) => {
-    document.querySelectorAll(".color-wrap div").forEach((div) => {
-      div.style.opacity = 1;
-    });
-
     document.querySelector(btn).addEventListener("click", () => {
-      document
-        .querySelectorAll(`.color-wrap div :not(${showSelector})`)
-        .forEach((div) => {
-          div.style.opacity = 0.1;
-        });
-
-      document.querySelectorAll(".pink-color").forEach((div) => {
-        div.style.width = "50px";
+      document.querySelectorAll(".color-wrap div").forEach((div) => {
+        div.style.display = "none"; // все цвета скрываются
       });
+      if (showSelector.length > 0) {
+        document.querySelectorAll(showSelector).forEach((div) => {
+          div.style.display = "block"; // показывается только нужный цвет
+          console.log("эта часть кода срабатывает");
+        });
+      } else {
+        document.querySelector(showSelector).style.display = "block"; // показывается только нужный цвет
+      }
     });
   };
 
   showColor(".pink", ".pink-color");
   showColor(".blue", ".blue-color");
   showColor(".yellow", ".yellow-color");
-  showColor(".green", ".pink-color");
+  showColor(".green", ".green-color");
+  showColor(".all-collors", ".color-wrap div");
 };
