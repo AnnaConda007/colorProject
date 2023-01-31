c9844d3556651a903416d4047aca506bd80255a5
Anna Hrulckova <annahrulkova@yandex.ru>
2023-01-31 19:03:01 +0400

    при нведении на блок с цветами изображения меняются, но не увеличиваются

--- js/main.js
+++ js/main.js
@@ -1,7 +1,46 @@
 // вызов функций
-window.addEventListener("DOMContentLoaded", () => {
-  "use strict";
-});
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict'
+})
 
 // modals
-const modal = () => {};
+ // изменение изображения при наведении 
+const chengeImg=({img, newLink, lastLink})=>{
+  const showNewImg = (imgSelector, newImgLink) => {
+    const img = document.querySelectorAll(imgSelector).forEach((img) => {
+        img.addEventListener('mouseenter', () => {
+          img.classList.add("active-img")// увеличение изображения, не работает, так как перебивается стилем на 135 строке
+          img.setAttribute("src", newImgLink)
+        })
+      })
+  }
+  const hideNewImg = (imgSelector, lastImgLink) => {
+    const img = document.querySelectorAll(imgSelector).forEach((img) => {
+        img.addEventListener('mouseleave', () => {
+          img.classList.remove("active-img")
+          img.setAttribute("src", lastImgLink)
+        })
+      })
+  }
+  showNewImg(img, newLink)
+  hideNewImg(img, lastLink)
+   
+}
+
+chengeImg({img:".pink-color img", newLink:"img/salute.png",
+lastLink:"img/allColors/pink.png"})
+
+chengeImg({img:".blue-color img", newLink:"img/salute2.png",
+lastLink:"img/allColors/blue.png"})
+
+chengeImg({img:".green-color img", newLink:"img/salute3.png",
+lastLink:"img/allColors/green.png"})
+
+chengeImg({img:".yellow-color img", newLink:"img/salute4.png",
+lastLink:"img/allColors/yellow.png"})
+
+
+
+
+
+
