1a383455d3c81d551288a6ebe8646eb3419b11bd
Anna Hrulckova <annahrulkova@yandex.ru>
2023-02-06 16:38:19 +0400

    добавлены комментарии

--- js/main.js
+++ js/main.js
@@ -1,25 +1,54 @@
- const moreBtn = document.querySelector(".more-colors")
 
 
 
+
+var xhr = new XMLHttpRequest();//  создаем объект, хранящий в себе запрос
+xhr.open("GET", "color.json" );// "открываем" запрос, настраиваем - тип, адрес, по которому нужно обратиться
+// 6 строкой, ЗАРАНЕЕ задается команда - собрать все что есть по указанному адресу.
+// По умолчанию данные сохраняются в объект xhr в том же формате,в котором данные храняться по указанному в запросе адресу 
+
+xhr.responseType = "json"; // ЗАРАНЕЕЕ меняем тип данных после сохранения в переменную(объект). Пока запрос еще не соверщается 
+
+xhr.onreadystatechange = ()=>{// запрос пока так же не запускается, тут ЗАРАНЕЕ задаются все команды, которые должны выполнится при каждом изменении статуса запроса
+// событие onreadystatechange в данном случае выполнится более  раза, следовательно и данные выведутся несколько раз?
+ let response = xhr.response
+ for (key in response){
+console.log(response[key].title)
+}
+} 
+
+xhr.send();// запрос отправляется, запускются все команды описанные выше.
+
+
+
+
+
+
+/*let arr = {
+  one : 1,
+two:2,
+three :3
+
+}
  
+for (key in arr){
+  console.log(key)
+console.log(arr[key])
+}
+const moreBtn = document.querySelector(".more-colors")
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
-xhr.responseType = "json";
-xhr.onreadystatechange = function() {
-
-  let response = xhr.response
-  console.log(response[1]);
-
-  for (let key in response) { 
-  }
+*/ 
   
-};
-xhr.send();
 
 
+/*
 
+xhr.onreadystatechange = function() {
+if(this.readyState==4 && this.status==200){
+  
+}
+*/
+//xhr.responseType = "json";
 /*
 
  moreBtn.addEventListener("click",()=>{
