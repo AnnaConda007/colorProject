00ff36d6851de17aa94394b286fd8dc6e54d3e5b
Anna Hrulckova <annahrulkova@yandex.ru>
2023-02-09 18:01:09 +0400

    test

--- js/main.js
+++ js/main.js
@@ -33,3 +33,21 @@ filterBlock = () => {
 
 
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted", formData.toString() ))
+    .catch((error) => alert(error));
+};
+
+document
+  .querySelector("form")
+  .addEventListener("submit", handleSubmit);
